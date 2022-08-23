d6d046a70af0b7ae8a2f38cbf3cc39a8404ca43b
Qureshi <Qureshi@DESKTOP-471LT58>
2022-08-22 21:33:07 -0700

    CORs Added Succesfully

--- InspectionAPI/Program.cs
+++ InspectionAPI/Program.cs
@@ -1,6 +1,8 @@
 using InspectionAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
+var myAllowSpecificOrigions = "_myAllowSpecificOrigions";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,8 +13,22 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-var app = builder.Build();
 
+//Enable CRoss Origin Resource Sharing CORs
+builder.Services.AddCors(Options =>
+{
+    Options.AddPolicy(name: myAllowSpecificOrigions,
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+
+        });
+}
+);
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -21,7 +37,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(myAllowSpecificOrigions);
 app.UseAuthorization();
 
 app.MapControllers();
