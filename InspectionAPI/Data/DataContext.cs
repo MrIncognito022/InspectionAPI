90ff365dbbaa27c373c92191377c88efdaa02d3d
Qureshi <Qureshi@DESKTOP-471LT58>
2022-08-22 21:19:41 -0700

    DbContext Added

--- InspectionAPI/Data/DataContext.cs
+++ InspectionAPI/Data/DataContext.cs
@@ -1,6 +1,15 @@
-﻿namespace InspectionAPI.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InspectionAPI.Data
 {
-    public class DataContext:DbContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Inspection> Inspections { get; set; }
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+        public DbSet<Status> Statuses { get; set; }
     }
 }
